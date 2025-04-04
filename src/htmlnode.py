bb7f5153c93d5f39c3aa7b8203cb727c93ca17a0
MaryKateMagyar <marykatemagyar@gmail.com>
2025-04-04 10:53:36 -0700

    create extract_title function as well as template and index files for page contents

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -193,5 +193,14 @@ def markdown_to_html_node(markdown):
 
     return ParentNode("div", children)
 
+def extract_title(markdown):
+    lines = markdown.split("\n")
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith("# "):
+            return line[2:].strip()
+
+    raise Exception("No h1 header found")
         
         
