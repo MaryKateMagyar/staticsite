dc9d28a403aa13b142e642e50d2fc672cd1c5703
MaryKateMagyar <marykatemagyar@gmail.com>
2026-01-05 13:29:18 -0800

    added text to textnodes function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from htmlnode import LeafNode
 
 
@@ -175,6 +175,22 @@ class TestTextNode(unittest.TestCase):
             ],
             new_nodes
         )
+
+    def test_text_to_text_nodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertListEqual(text_to_textnodes(text), result)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -119,3 +119,12 @@ def split_nodes_link(old_nodes):
                     new_nodes.append(TextNode(text, TextType.TEXT))
 
     return new_nodes
+
+def text_to_textnodes(text):
+    starting_nodes = [TextNode(text, TextType.TEXT)]
+    bold_nodes = split_nodes_delimiter(starting_nodes, "**", TextType.BOLD)
+    italic_nodes = split_nodes_delimiter(bold_nodes, "_", TextType.ITALIC)
+    code_nodes = split_nodes_delimiter(italic_nodes, "`", TextType.CODE)
+    image_nodes = split_nodes_image(code_nodes)
+    final_nodes = split_nodes_link(image_nodes)
+    return final_nodes
